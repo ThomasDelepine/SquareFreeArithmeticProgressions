fac52157287df5b12d42b5bb12a02fef55f1377e
ThomasDelepine <thomas.delepine@live.fr>
2026-01-07 18:26:50 +0100

    lemma_A.cpp v2

--- lemma_A.cpp
+++ lemma_A.cpp
@@ -18,6 +18,9 @@ Code to assert that
 	- the pattern (a)⋄^d(b) is (27, h26)-recurrent for d in {1, 2, 4, 5, 6, 7, 8}, and
 	- the pattern ab is (12, h26)-recurrent for a != b
 
+To do so, we generate all possible factors of length 60 of h(t) for every infinite ternary square-free word t
+(so in particular, it is enough to generate the factors for every square-free words of length 4)
+
 compilation : g++ -O3 -march=native -flto -g -o lemma_A.o lemma_A.cpp
 execution   : ./lemma_A.o
 
@@ -26,88 +29,21 @@ execution   : ./lemma_A.o
 		h26(2) = 20102101210212010210120102
 */
 
-bool checkSquareFreeAtEnd(std::string& s, int l){
-    /*
-    Assume that s = uv with u square free and |v| = l, return true iff s is square free
-    */
-    if(s.size() <= l)
-    	return true;
-    for(int period = 1; period <= s.size()/2; period++){
-        for(int end = 0; end < std::min(2*period - 1, l); end++){
-            int end_pos = s.size() - l + end;
-            bool flag = true;
-            for(int i = 0; i < period; i++ ){             
-                if(s[end_pos - i] != s[end_pos - i - period]){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag){
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-
-
-std::pair<bool, int> has_two_patterns(const std::string& s, const char a, const char b, const int d){
-	/*
-	returns (false, -1) if s does not have the pattern (a)⋄^d(b) 
-	returns (true, -1)  if s has exactly one occurence of the pattern (a)⋄^d(b) 
-	returns (true, dist)   if s has at least two occurences of the pattern and dist is the distance between the two leftmost patterns (a)⋄^d(b) 
-	*/
-
-	// First, we find a first pattern
-	bool has_one_pattern = false;
-	int pos_first_pattern;
-	for(int i = 0; i + d + 1 < s.size(); i++){
-		if(s[i] == a && s[i + d + 1] == b){
-			has_one_pattern =  true;
-			pos_first_pattern = i;
-			break;
-		}
-	}
-
-	// If there is a pattern strating at pos_first_pattern :
-	if(has_one_pattern){
-		for(int i = pos_first_pattern + 1; i + d + 1 < s.size(); i++){
-			// If we find a second pattern
-			if(s[i] == a && s[i + d + 1] == b){
-				return std::pair<bool, int>(true, i - pos_first_pattern);
-			}
-		}
-		// Otherwise there is no second pattern in s
-		return std::pair<bool, int>(true, -1);
-	}
-
-	// Otherwise there is no pattern in s :
-	return std::pair<bool, int>(false, -1);
-}
-
-void max_distance_between_patterns(const char a, const char b, int d, std::set<std::string>& factors){
-	/*
-	Returns the maximum distance between the two left-most patterns (a)⋄^d(b) amongst every factor in factors
-
-	If one factor has at most 1 such pattern, we assert(false) (should not be triggered)
-	*/
-	int largest = 0;
-	std::string largest_str = "";
-	// Loop over all factors that we are interested in
-	for(std::string s : factors){
-		std::pair<bool, int> tmp = has_two_patterns(s, a, b, d);
-		if(!tmp.first || tmp.second == -1){
-			assert(false); 
-		}
-		else{
-			if(largest < tmp.second){
-				largest = tmp.second;
-				largest_str = s;
+std::vector<std::string> all_squares_of_size_4(){
+	std::vector<std::string> res;
+	for(const std::string a : {"0", "1", "2"}){
+		for(const std::string b : {"0", "1", "2"}){
+			for(const std::string c : {"0", "1", "2"}){
+				for(const std::string d : {"0", "1", "2"}){
+					// abcd is not square-free if and only if a=b or b=c or c=d (square of period 1) or abcd = abab (square of period 2)
+					if(a != b && b != c && c != d && (a != c || b != d)){
+						res.push_back(a+b+c+d);
+					}
+				}
 			}
 		}
 	}
-	std::cout << "For the pattern (" << a << ") ⋄^" << d << " (" << b << "), the largest distance is " << std::to_string(largest) << std::endl;
+	return res;
 }
 
 void all_factors(const std::string& s, const int size, std::set<std::string>& acc){
@@ -123,73 +59,49 @@ void all_factors(const std::string& s, const int size, std::set<std::string>& ac
 	}
 }
 
-std::set<std::string> images_of_h26(const int size_for_factors, const int number_of_images){
-	/*
-
-	*/
-	// The images of h26 						h26(0)							h26(1) 						h26(2)
+std::set<std::string>factors(const int size_for_factors){
 	const std::vector<std::string> h26 = {"01210212021020121021201210", "12021020102101202102012021", "20102101210212010210120102"};
-	// The set of factors
 	std::set<std::string> res;
-	// Initialization of the data structure for the search
-	std::string s = "";				// the current string
-	std::stack<std::string> stack;	// the stack to emulate a DFS
-	for(std::string image : h26)
-		stack.push(image);
-	stack.push(h26[2]);
-	stack.push(h26[1]);
-	stack.push(h26[0]);
-
-	// while the current word can be extended
-	while(!stack.empty()){
-		// Depile case
-		if(stack.top() == "p"){
-			stack.pop();
-			for(int i = 0; i < 26; i++){
-				s.pop_back();
-			}
-		}
-		// Extension case
-		else{
-			s = s + stack.top();
-			stack.pop();
-			// If adding 26 new letters did not create any square :
-			if(checkSquareFreeAtEnd(s, 26)){
-				// if we've reached the aimed size :
-				if(s.size() >= 26*number_of_images){
-					// we store the factors
-					all_factors(s, size_for_factors, res);
-					// then we backtrack
-					for(int i = 0; i < 26; i++){
-						s.pop_back();
-					}
-				}
-				// Otherwise, we continue to extend the current word
-				else{
-					stack.push("p");
-					stack.push(h26[2]);
-					stack.push(h26[1]);
-					stack.push(h26[0]);
-				}
-			}
-			// If we created a square, then we backtrack (and pop_back 26 times)
-			else{
-				for(int i = 0; i < 26; i++){
-					s.pop_back();
-				}
-			}
-		}
+	for(const std::string s : all_squares_of_size_4()){
+		all_factors(h26[s[0] - '0'] + h26[s[1] - '0'] + h26[s[2] - '0'] + h26[s[3] - '0'], size_for_factors, res);
 	}
 	return res;
 }
 
+int max_distance_between_patterns(const char a, const char b, int d, std::set<std::string>& factors){
+	/*
+	Returns the maximum distance between the two left-most patterns (a)⋄^d(b) amongst every factor in factors
+
+	If one factor has at most 1 such pattern, the function does not terminate (should always terminate)
+	*/
+	int pos1, pos2, max;
+	max = 0;
+	for(const std::string s : factors){
+		int cpt = 0;
+		// we find the first occurence of the pattern
+		while(s[cpt] != a || s[cpt + d + 1] != b){
+			cpt++;
+		}
+		pos1 = cpt;
+		cpt++;
+		// we find the second occurence of the pattern
+		while(s[cpt] != a || s[cpt + d + 1] != b){
+			cpt++;
+		}
+		pos2 = cpt;
+		max = std::max(max, pos2 - pos1);
+	}
+	return max;
+}
+
 int main() {
 	std::cout << "The largest distance should be 28 (hence (27,h)-recurrence)" << std::endl;
-	std::set<std::string> images_of_h26_set = images_of_h26(200, 9);
+	std::set<std::string> factors_set = factors(60);
 	for(char a : {'0', '1', '2'}){
 		for(char b : {'0', '1', '2'}){
 			for(const int d : {1,2,4,5,6,7,8}){
-				max_distance_between_patterns(a, b, d, images_of_h26_set);
+				std::cout << "For the pattern (" << a << ") ⋄^" << d << " (" << b << "), the largest distance is ";
+				std::cout << max_distance_between_patterns(a, b, d, factors_set) << std::endl;
 			}
 		}
 	}
@@ -197,7 +109,8 @@ int main() {
 	for(char a : {'0', '1', '2'}){
 		for(char b : {'0', '1', '2'}){
 			if(a != b){
-				max_distance_between_patterns(a, b, 0, images_of_h26_set);
+				std::cout << "For the pattern (" << a << ") ⋄^" << 0 << " (" << b << "), the largest distance is ";
+				std::cout << max_distance_between_patterns(a, b, 0, factors_set) << std::endl;
 			}
 		}
 	}	
